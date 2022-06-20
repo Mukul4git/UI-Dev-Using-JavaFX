7eccaebf0941b83904695e31deb11519a5da024a
Mukul4git <mukulhooda2002@gmail.com>
2022-06-19 22:17:13 -0400

    Created and Tested Working of Table and Chart view by entering Mock Data.

--- src/main/java/ca/georgiancollege/comp1011assignment1/GraphViewController.java
+++ src/main/java/ca/georgiancollege/comp1011assignment1/GraphViewController.java
@@ -2,11 +2,22 @@ package ca.georgiancollege.comp1011assignment1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-public class GraphViewController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class GraphViewController implements Initializable {
 
     @FXML
     private BarChart<String, Integer> barChart;
@@ -17,9 +28,37 @@ public class GraphViewController {
     @FXML
     private NumberAxis NumberAxis;
 
+    //Defining the OnCLick Action to Change the Scene.
     @FXML
-    void LoadTableView(ActionEvent event) {
+    void LoadTableView(ActionEvent event) throws IOException {
+
+        Parent part = FXMLLoader.load(getClass().getResource("Table-view.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(part);
+        stage.setScene(scene);
+        stage.show();
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //Initializing Series for mock data.
+        XYChart.Series series1 = new XYChart.Series();
+
+        series1.setName("2020");
+
+        //Mock Data for BarChart
+
+        series1.getData().add(new XYChart.Data("India", 12000));
+        series1.getData().add(new XYChart.Data("Australia", 8000));
+        series1.getData().add(new XYChart.Data("China", 98000));
+        series1.getData().add(new XYChart.Data("Canada", 67000));
+        series1.getData().add(new XYChart.Data("USA", 34000));
+
+        //Adding data into the Chart
+        barChart.getData().addAll(series1);
+
+
+    }
 }
--- src/main/java/ca/georgiancollege/comp1011assignment1/TableViewController.java
+++ src/main/java/ca/georgiancollege/comp1011assignment1/TableViewController.java
@@ -2,11 +2,18 @@ package ca.georgiancollege.comp1011assignment1;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,6 +36,18 @@ public class TableViewController implements Initializable{
 
     @FXML
     private TableColumn<Country, String> Region;
+
+    //Defining the OnCLick Action to Change the Scene.
+    @FXML
+    void LoadChartView(ActionEvent event) throws IOException {
+
+        Parent part = FXMLLoader.load(getClass().getResource("Graph-view.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(part);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
