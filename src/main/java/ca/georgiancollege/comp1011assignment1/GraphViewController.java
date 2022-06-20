6d963fdb871aeee024651b536acfd3806735f2df
Mukul4git <mukulhooda2002@gmail.com>
2022-06-19 21:30:31 -0400

    Created GraphView fxml file and defined the axis

--- src/main/java/ca/georgiancollege/comp1011assignment1/GraphViewController.java
+++ src/main/java/ca/georgiancollege/comp1011assignment1/GraphViewController.java
@@ -1,4 +1,25 @@
 package ca.georgiancollege.comp1011assignment1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
 public class GraphViewController {
+
+    @FXML
+    private BarChart<String, Integer> barChart;
+
+    @FXML
+    private CategoryAxis CategoryAxis;
+
+    @FXML
+    private NumberAxis NumberAxis;
+
+    @FXML
+    void LoadTableView(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/ca/georgiancollege/comp1011assignment1/Main.java
+++ src/main/java/ca/georgiancollege/comp1011assignment1/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Table-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Graph-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
